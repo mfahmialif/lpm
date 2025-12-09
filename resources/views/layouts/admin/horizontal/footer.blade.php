18decfcad5ab6b4c456bf4b11d7024627fa84338
mfahmialif <mfahmialif@gmail.com>
2025-12-09 23:00:56 +0700

    update footer

--- resources/views/layouts/admin/horizontal/footer.blade.php
+++ resources/views/layouts/admin/horizontal/footer.blade.php
@@ -1,4 +1,3 @@
-<!-- Footer -->
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl">
         <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
@@ -8,17 +7,11 @@
                     document.write(new Date().getFullYear());
 
                 </script>
-                , made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
+                , made with ❤️ by <a href="#" class="footer-link">LPM</a>
             </div>
             <div class="d-none d-lg-inline-block">
-                <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
-                <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More Themes</a>
-
-                <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
-
-                <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+                <a href="#" class="footer-link me-4">LPM IT</a>
             </div>
         </div>
     </div>
 </footer>
-<!-- / Footer -->
