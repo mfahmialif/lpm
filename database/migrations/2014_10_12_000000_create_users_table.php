02162d3c86b3543ad329a15b25788a878b27d220
mfahmialif <mfahmialif@gmail.com>
2025-12-20 21:55:53 +0700

    update user migrations add unit role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->enum('sex', ['Male', 'Female', '*']);
-            $table->enum('role', ['admin', 'user', 'staff']);
+            $table->enum('role', ['admin', 'user', 'staff', 'unit']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('telp')->nullable();
